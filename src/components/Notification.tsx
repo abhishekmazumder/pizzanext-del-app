df031df7ce609d599b0fe9ddaab1af8663265ae1
Abhishek Mazumder <abhishekmazumder69@gmail.com>
2023-08-26 12:11:32 +0530

    Notificatin bar added

---
+++ src/components/Notification.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div className='h-12 bg-red-500 text-white flex items-center justify-center text-center text-sm md:text-base'>Free delivery for all orders above Rs.500. Order Now!</div>
+  )
+}
+
+export default Notification
